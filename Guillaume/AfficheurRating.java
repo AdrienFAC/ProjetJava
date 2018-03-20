3c8b2484114289a2fabac6523dd525004f17272a
guitou810 <37578710+guitou810@users.noreply.github.com>
2018-03-20 10:58:19 +0000

    Add files via upload

---
+++ Guillaume/AfficheurRating.java
@@ -0,0 +1,23 @@
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Panel;
+import java.awt.TextField;
+
+public class AfficheurRating extends Panel {
+		TextField t1;
+	
+
+
+	public AfficheurRating(ImageModel m){
+		this.setLayout(new FlowLayout());
+		Panel p = new Panel();
+		t1 = new TextField("/20");
+		add(t1);	
+			
+		}
+
+		
+	}
+
+	
+
