6fccacdcb1bf072af55ff65f267262aefe02b03f
cam0202 <37930686+cam0202@users.noreply.github.com>
2018-04-08 16:09:06 +0200

    Modification tri et création de la gestion image

--- Camille/src/Camille/GestionImg.java
+++ Camille/src/Camille/GestionImg.java
@@ -2,11 +2,55 @@ package Camille;
 
 import java.util.List;
 
-public class GestionImg {
-	
-	//List<TriList> liste;
-	
-	public GestionImg() {
+import Adrien.ImageModel;
+import Guillaume.AfficheurImage;
+import Guillaume.Afficheurtri;
 
+public class GestionImg  {
+	
+	public List<ImageModel> changementImg; 
+	AfficheurImage imageActuelle;
+	int  suivant;
+	int precedent;
+	
+	public GestionImg(TriList listImg, Afficheurtri boutton ) {
+		
+		this.changementImg = (List<ImageModel>) listImg; 	
+		suivant = changementImg.indexOf(imageActuelle.im_default) + 1;
+		precedent = changementImg.indexOf(imageActuelle.im_default) - 1;
+		
 	}
+	
+	public int gestionSuivant() {
+		
+		if(suivant == changementImg.size()) {
+			return  0;
+		} else {
+			return changementImg.indexOf(imageActuelle.im_default) + 1; 
+		}
+		
+	}
+	
+	public void changeImgSuivant() {
+		imageActuelle.im_default = "images/" + changementImg.get(suivant).titre + "jpg";
+		suivant = gestionSuivant();
+	}
+	
+	public int gestionPrecedent() {
+		
+		if(precedent == 0) {
+			return  changementImg.size() - 1;
+		} else {
+			return changementImg.indexOf(imageActuelle.im_default) - 1; 
+		}
+		
+	}
+	
+	public void changeImgPrecedent() {
+		System.out.println("coucou" );
+		//appli.im_default = "images/" +  changementImg.get(precedent).titre +".jpg";
+		//precedent = gestionPrecedent();
+	}
+	
+	
 }
--- Camille/src/Camille/TriList.java
+++ Camille/src/Camille/TriList.java
@@ -7,44 +7,43 @@ import Adrien.MainModel;
 import Guillaume.Afficheurtri;
 
 public class TriList {
-	
+
 	public List<ImageModel> choixImg;
-	
-	public TriList(MainModel lst, Afficheurtri appli) {
-		
+	Afficheurtri appli;
+
+
+	public TriList(MainModel lst) {
+
 		this.choixImg = lst.lst_images; 
-		
-		for(int i = 0; i < choixImg.size(); i++) {
-			
-			// Adaptation de la liste a la taille.
-			if(choixImg.get(i).taille.equals(appli.taille) == false) {
-				choixImg.remove(i);
-			}
-		
-			// Aux titres
-			if(choixImg.get(i).titre.equals(appli.titre) == false ) {
-				choixImg.remove(i);
-			}
-			
-			// Aux tags
-				// Parcourt des tags de l'image.
-			int j = 0;
-			boolean estPresent = false;
-			while(j < choixImg.get(i).lst_tags.size() && estPresent == false) {
-				if(choixImg.get(i).lst_tags.get(j).equals(appli.tag)) { // regarde si le tag sélectionner est associé a l'image.
-					estPresent = true;
-				}
-			}
-				// Suppression de l'image si le tag n'est pas prèsent.
-			if(estPresent == false) {
-				choixImg.remove(i);
-			}
-			
-		}
-		
 	}
-	
-	
-	
-	
+
+	public void choixTaille() {
+
+		for(int i = 0; i < choixImg.size(); i++) {
+			if(this.choixImg.get(i).taille.equals(appli.taille) == false) {
+				this.choixImg.remove(i);
+			}	
+		}
+
+	}
+
+	public void choixTitre() {
+
+		for(int i = 0; i < this.choixImg.size(); i++) {
+			if(this.choixImg.get(i).titre.contains((CharSequence) appli.titre) == false ) {
+				this.choixImg.remove(i);
+			}
+		}
+	}
+
+	public void choixTag() {
+		for(int i = 0; i < this.choixImg.size(); i++) {
+			if(this.choixImg.get(i).lst_tags.contains(appli.tag) == false) {
+				this.choixImg.remove(i);
+			}
+		}
+	}
+
+
+
 }
